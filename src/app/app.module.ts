14eb4c106d2e5e970de33d2362c28c0e7e4d231c
Swich313 <andruha313@gmail.com>
2023-07-06 22:39:42 +0300

    completed details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { ProfitPipe } from './pipes/profit.pipe';
 import {FormsModule} from "@angular/forms";
+import { DetailsActorsComponent } from './components/details-actors/details-actors.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import {FormsModule} from "@angular/forms";
     SearchComponent,
     LoaderComponent,
     ModalComponent,
-    ProfitPipe
+    ProfitPipe,
+    DetailsActorsComponent
   ],
   imports: [
     BrowserModule,
